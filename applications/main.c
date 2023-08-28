db10f70a8126125c157dc6cb533c2d413c46bf40
MagicLOH <toyzloh@gmail.com>
2023-08-28 21:45:58 +0800

    08-28 21:40, adapt fal component done.

--- applications/main.c
+++ applications/main.c
@@ -24,9 +24,9 @@ int main(void)
     while (count++)
     {
         rt_pin_write(LED0_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
+        rt_thread_mdelay(200);
         rt_pin_write(LED0_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
+        rt_thread_mdelay(200);
     }
 
     return 0;
